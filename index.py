31cf816ad126de8d2bc368f51eeac47b7a080978
cognosx <162955185+cognosx@users.noreply.github.com>
2024-03-18 03:44:17 -0400

    Update index.py

--- index.py
+++ index.py
@@ -24,4 +24,4 @@ def display_page(pathname):
         return '404'
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
